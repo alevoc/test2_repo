2f13468246ee3936488f35cd65956a9fadd322f8
alevoc <Alex N>
2024-03-11 15:50:18 +0300

    Modify git_init.sh

---
+++ git_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git init
+echo -e "2branches.sh\ngit_init.sh" > .gitignore
+git add .gitignore
+git commit -m "Add .gitignore"
+git config user.email "anikolayev@mail.ru"
+git config user.email "Alex N"
